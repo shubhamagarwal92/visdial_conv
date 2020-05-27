15f96db50ab058d6b9c907bb777cdd0f2fd9ed93
Shubham Agarwal <shubhamagarwal92@gmail.com>
2020-05-27 23:42:53 +0100

    allowing gru for dynamic rnn

--- visdialch/utils/dynamic_rnn.py
+++ visdialch/utils/dynamic_rnn.py
@@ -1,12 +1,20 @@
 import torch
 from torch import nn
+from torch.autograd import Variable
+from torch.nn import functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 class DynamicRNN(nn.Module):
-    def __init__(self, rnn_model):
+    # SA: DynamicRNN now supports GRU cell. Defaults to LSTM.
+    # Bidirectional RNN funcitonality also added.
+    # def __init__(self, rnn_model, rnn_type='GRU', bi_enc=True):
+    def __init__(self, rnn_model, rnn_type='LSTM', bi_enc=False):
         super().__init__()
         self.rnn_model = rnn_model
+        self.rnn_type = rnn_type
+        self.bi_enc = bi_enc
+
 
     def forward(self, seq_input, seq_lens, initial_state=None):
         """A wrapper over pytorch's rnn to handle sequences of variable length.
@@ -23,7 +31,7 @@ class DynamicRNN(nn.Module):
 
         Returns
         -------
-            Single tensor of shape (batch_size, rnn_hidden_size) corresponding
+            A single tensor of shape (batch_size, rnn_hidden_size) corresponding
             to the outputs of the RNN model at the last time step of each input
             sequence.
         """
@@ -31,32 +39,58 @@ class DynamicRNN(nn.Module):
         sorted_len, fwd_order, bwd_order = self._get_sorted_order(seq_lens)
         sorted_seq_input = seq_input.index_select(0, fwd_order)
         packed_seq_input = pack_padded_sequence(
-            sorted_seq_input, lengths=sorted_len, batch_first=True
-        )
+            sorted_seq_input, lengths=sorted_len, batch_first=True)
 
         if initial_state is not None:
             hx = initial_state
+            sorted_hx = [x.index_select(1, fwd_order) for x in hx]
             assert hx[0].size(0) == self.rnn_model.num_layers
         else:
             hx = None
 
         self.rnn_model.flatten_parameters()
-        outputs, (h_n, c_n) = self.rnn_model(packed_seq_input, hx)
+
+        # SA: supporting dynamic GRU cell
+        if self.rnn_type == 'LSTM':
+            outputs, (h_n, c_n) = self.rnn_model(packed_seq_input, hx)
+        else:
+            # SA: GRU cell doesn't have c_n. LSTM returns tuple.
+            outputs, h_n = self.rnn_model(packed_seq_input, hx)
+            c_n = None
+        # SA: Bi-directional RNN functionality
+        # If not bidirectional, we will have hidden as (L, B, D).
+        # Converting it to same format.
+        if self.bi_enc:
+            # From RNN we have hidden as (Layer*directions, Batch, Dim)
+            # First Convert it to (Layer, Batch, Dim*directions)
+            # dim_0 = layer*directions
+            # odd index -> forward, even -> backward. concat together
+            dim_0 = h_n.size(0)
+            h_n = torch.cat([h_n[0:dim_0:2], \
+                                h_n[1:dim_0:2]], 2)
+        # SA(explanation): Take the hidden from last layer and
+        # reorder them back
+        # rnn_output = h_n[-1].index_select(dim=0, index=bwd_order)
+        # return rnn_output
+
+
+        # outputs, (h_n, c_n) = self.rnn_model(packed_seq_input, hx)
 
         # pick hidden and cell states of last layer
         h_n = h_n[-1].index_select(dim=0, index=bwd_order)
-        c_n = c_n[-1].index_select(dim=0, index=bwd_order)
+        if self.rnn_type == 'LSTM':
+            c_n = c_n[-1].index_select(dim=0, index=bwd_order)
 
-        outputs = pad_packed_sequence(
+        outputs_tuple = pad_packed_sequence(
             outputs, batch_first=True, total_length=max_sequence_length
         )
+        outputs = outputs_tuple[0].index_select(dim=0, index=bwd_order)
+        # Outputs -> tuple of (tensor, grad_fn), return tensor
         return outputs, (h_n, c_n)
 
     @staticmethod
     def _get_sorted_order(lens):
-        sorted_len, fwd_order = torch.sort(
-            lens.contiguous().view(-1), 0, descending=True
-        )
+        sorted_len, fwd_order = torch.sort(lens.contiguous().view(-1), 0, descending=True)
         _, bwd_order = torch.sort(fwd_order)
         sorted_len = list(sorted_len)
         return sorted_len, fwd_order, bwd_order
