aa3866ebe968c2150e1e65a198ee3f743a7289c6
Shubham Agarwal <shubhamagarwal92@gmail.com>
2020-09-25 15:19:43 +0530

    Updating init

--- visdialch/encoders/__init__.py
+++ visdialch/encoders/__init__.py
@@ -1,6 +1,13 @@
 from visdialch.encoders.lf import LateFusionEncoder
-
+from visdialch.encoders.mcan_img_only import MCANImgOnlyEncoder
+from visdialch.encoders.mcan_img_mcan_vqa_hist_attn import MCANImgMCANVQAHistAttnEncoder
+from visdialch.encoders.mcan_img_mcan_hist import MCANImgMCANHistEncoder
+from visdialch.encoders.mcan_hist_only import MCANHistOnlyEncoder
 
 def Encoder(model_config, *args):
-    name_enc_map = {"lf": LateFusionEncoder}
+    name_enc_map = {"lf": LateFusionEncoder,
+                    "mcan_img_mcan_hist":MCANImgMCANHistEncoder,
+                    "mcan_hist_only": MCANHistOnlyEncoder,
+                    "mcan_img_mcan_vqa_hist_attn": MCANImgMCANVQAHistAttnEncoder,
+                    "mcan_img_only": MCANImgOnlyEncoder}
     return name_enc_map[model_config["encoder"]](model_config, *args)
